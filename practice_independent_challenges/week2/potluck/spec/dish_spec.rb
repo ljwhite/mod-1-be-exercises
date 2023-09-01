db52ec087f338384d995b0233968a9dc5cddb3be
Lito White <leland.white@gmail.com>
2023-09-01 11:43:28 -0600

    week2 iteration complete

--- practice_independent_challenges/week2/potluck/spec/dish_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 require './lib/dish'
+require './lib/potluck'
+require 'pry'
 
 describe Dish do
     before(:each) do 
@@ -17,5 +19,38 @@ describe Dish do
         it 'has a category' do
             expect(@dish.category).to eq :appetizer
         end
+    end
+end
+
+describe Potluck do 
+    before(:each) do 
+        date = "7-13-18"
+        @potluck = Potluck.new(date)
+    end
+
+    describe "setup" do 
+        it 'is a potluck' do 
+            expect(@potluck).to be_a Potluck 
+        end
+        it 'has a date' do 
+            expect(@potluck.date).to eq("7-13-18")
+        end
+        it 'has comes with no set dishes' do 
+            expect(@potluck.dishes).to eq([])
+        end
+    end
+
+    describe 'potluck can have dishes' do 
+        before(:each) do
+            @couscous_salad = Dish.new("Couscous Salad", :appetizer)
+            @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        end
+        it 'can add dishes' do 
+            @potluck.add_dish(@couscous_salad)
+            @potluck.add_dish(@cocktail_meatballs)
+            expect(@potluck.dishes).to be_a Array
+            expect(@potluck.dishes.length).to eq(2)
+        end
+        
     end
 end
