1abc58c8df505bffd56f52e64553dfc6b513648b
Lito White <leland.white@gmail.com>
2023-09-01 19:24:18 -0600

    week2 assessment complete

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -1,3 +1,5 @@
+require './lib/dish'
+
 class Potluck
 
     attr_reader :date, :dishes
@@ -10,4 +12,26 @@ class Potluck
     def add_dish(dish)
         dishes << dish
     end
+
+    def get_all_from_category(category)
+        dishes.find_all {|dish| dish.category == category}
+    end
+
+    def menu
+        group = dishes.group_by {|dish| dish.category}
+        menu = Hash.new{|h,key| h[key] = []}
+        group.each do |category, dishes|
+            dishes.each do |dish|
+                menu[category] << dish.name
+            end
+        end
+        menu
+    end
+
+    def ratio(category)
+        num = get_all_from_category(category).length
+        den = dishes.count
+        (num.to_f / den)*100.round(1)
+    end
 end
+
--- practice_independent_challenges/week2/potluck/spec/dish_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -51,6 +51,45 @@ describe Potluck do
             expect(@potluck.dishes).to be_a Array
             expect(@potluck.dishes.length).to eq(2)
         end
-        
+    end
+
+    describe 'potluck can breakdown dishes by category' do 
+        before(:each) do
+            @couscous_salad = Dish.new("Couscous Salad", :appetizer)
+            @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+            @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+            @roast_pork = Dish.new("Roast Pork", :entre)
+            @candy_salad = Dish.new("Candy Salad", :dessert)
+            @potluck.add_dish(@couscous_salad)
+            @potluck.add_dish(@cocktail_meatballs)
+            @potluck.add_dish(@summer_pizza)
+            @potluck.add_dish(@roast_pork)
+            @potluck.add_dish(@candy_salad)
+        end 
+
+        it 'can separate dishes by category' do 
+            result = @potluck.get_all_from_category(:appetizer)
+            expect(result).to be_a Array 
+            expect(result.first).to be_a Dish 
+            expect(result.last.category).to eq(:appetizer)
+            expect(result.first.name).to eq("Couscous Salad")
+        end
+
+        it 'can summarize a menu by category' do 
+            result = @potluck.menu
+            expect(result).to be_a Hash
+            expect(result[:appetizer]).to be_a Array
+            expect(result[:appetizer]).to include "Couscous Salad"
+            expect(result[:appetizer]).to include "Summer Pizza"
+            expect(result[:appetizer]).not_to include "Roast Pork"
+            expect(result[:appetizer].length).to eq 2
+        end
+
+        it 'can take a ratio from categories' do 
+            bean_dip = Dish.new("Bean Dip", :appetizer)
+            @potluck.add_dish(bean_dip)
+            expect(@potluck.ratio(:appetizer)).to eq(50.0)
+    
+        end
     end
 end
